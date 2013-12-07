305ff1360f06b62f580e9a7d046af71afb39e289
akhil <asquare@java.net>
2013-12-06 16:05:46 -0800

    upgrade to v0.10.21

--- src/test/java/net/java/libuv/VersionTest.java
+++ src/test/java/net/java/libuv/VersionTest.java
@@ -34,7 +34,7 @@ public class VersionTest extends TestBase {
     public void testVersion() {
         final String version = LibUV.version();
         System.out.println("libuv version is " + version);
-        Assert.assertEquals(version, "0.10.15");
+        Assert.assertEquals(version, "0.10.18");
     }
 
     @Test
