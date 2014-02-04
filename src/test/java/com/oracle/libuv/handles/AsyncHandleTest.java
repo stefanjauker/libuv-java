cdcadb358e6979e1388422ddf29d72c9f47efaac
akhil <asquare@java.net>
2014-02-03 19:01:11 -0800

    add test calling AsyncHandle.send from different thread

--- src/test/java/com/oracle/libuv/handles/AsyncHandleTest.java
+++ src/test/java/com/oracle/libuv/handles/AsyncHandleTest.java
@@ -72,9 +72,81 @@ public class AsyncHandleTest extends TestBase {
         Assert.assertEquals(times.get(), 1);
     }
 
+    static class Holder {
+        AsyncHandle asyncHandle;
+        volatile boolean initialized = false;
+        final int id;
+        final AtomicBoolean gotCallback = new AtomicBoolean(false);
+        final AtomicInteger times = new AtomicInteger(0);
+        final LoopHandle loop = new LoopHandle();
+        final Throwable[] errors = new Throwable[1];
+        final Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    asyncHandle = new AsyncHandle(loop);
+                    asyncHandle.setAsyncCallback(new AsyncCallback() {
+                        @Override
+                        public void onSend(final int status) throws Exception {
+                            gotCallback.set(true);
+                            System.out.println(id + " onSend!");
+                            times.incrementAndGet();
+                            asyncHandle.close();
+                        }
+                    });
+                    synchronized (loop) {
+                        initialized = true;
+                        loop.notifyAll();
+                    }
+                    loop.run();
+                } catch (Throwable throwable) {
+                    errors[0] = throwable;
+                }
+            }
+        };
+        Holder(int id) {this.id = id;}
+    }
+
+    @Test
+    public void testAsyncMulti() throws Throwable {
+        final int CONCURRENCY = 256;
+        final ScheduledExecutorService timer = new ScheduledThreadPoolExecutor(1);
+
+        final Holder[] holders = new Holder[CONCURRENCY];
+        for (int i=0; i < holders.length; i++) {
+            final Holder holder = holders[i] = new Holder(i);
+            holder.thread.start();
+            synchronized (holder.loop) {
+                while (!holder.initialized) {
+                    holder.loop.wait();
+                }
+            }
+            final int fi = i;
+            timer.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(fi + " calling asyncHandle.send...");
+                    holder.asyncHandle.send();
+                }
+            }, (int) (Math.random() * 1000), TimeUnit.MILLISECONDS);
+        }
+
+        for (int i=0; i < holders.length; i++) {
+            final Holder holder = holders[i];
+            holder.thread.join();
+            if (holder.errors[0] != null) {
+                throw holder.errors[0];
+            }
+
+            Assert.assertTrue(holder.gotCallback.get());
+            Assert.assertEquals(holder.times.get(), 1);
+        }
+    }
+
     public static void main(final String[] args) throws Throwable {
         final AsyncHandleTest test = new AsyncHandleTest();
         test.testAsync();
+        test.testAsyncMulti();
     }
 
 }
